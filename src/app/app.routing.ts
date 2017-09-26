cc5c3410080981d34a1b14d4a80ac95d11c16216
Lois Choi <loisch22@gmail.com>
2017-09-25 17:03:00 -0700

    follow epicodus example for setting up multiple components, set routes, use services, injection, providers

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,27 @@
-//all routes should be named app.routing.ts
-
-//pkg from Angular helps provide the router to the rest of the application
 import { ModuleWithProviders } from '@angular/core';
-
-//render specific components when specific URL are provided to the router
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'marketplace',
+    component: MarketplaceComponent
+  },
+  {
+    path: 'albums/:id',
+    component: AlbumDetailComponent
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
